c1b46dc010390db45df0bbcabb5c9309f1a659c1
deshack <deshack@linux.it>
2013-05-15 16:42:38 +0200

    Fix bootstrap.min.js path

--- footer.php
+++ footer.php
@@ -25,6 +25,6 @@
 <?php wp_footer(); ?>
 
 <script src="http://code.jquery.com/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 </body>
 </html>
