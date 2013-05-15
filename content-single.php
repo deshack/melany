7b83617872e5bcf893899fc15778acfac6e0f799
deshack <deshack@linux.it>
2013-05-15 13:46:55 +0200

    Setup single article meta info

--- content-single.php
+++ content-single.php
@@ -7,9 +7,16 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="page-header">
 		<h1><?php the_title(); ?></h1>
+		<?php edit_post_link( __( 'Edit', 'melany' ), '<button class="btn btn-small pull-right">', '</button>' ); ?>
 
 		<div class="entry-meta muted">
-			<small><?php melany_posted_on(); ?></small>
+			<small><?php melany_posted_on();
+					/* Check categories for this blog */
+					if( melany_categorized_blog() ){
+						$category_list = get_the_category_list( __( ', ', 'melany' ) );
+						printf( __( ' in %1$s', 'melany' ), $category_list );
+					}
+				?></small>
 		</div>
 	</header><!-- .page-header -->
 
@@ -26,38 +33,21 @@
 	<footer class="well">
 		<?php
 			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'melany' ) );
-
-			/* translators: used between list items, there is a space after the comma */
 			$tag_list = get_the_tag_list( '', __( ', ', 'melany' ) );
 
-			if ( ! melany_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'melany' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'melany' );
-				}
-
+			// This blog only has 1 category so we just need to worry about tags in the meta text
+			if ( '' != $tag_list ) {
+				$meta_text = __( '<span>Tags: %1$s</span><span class="pull-right">Bookmark the <a href="%2$s" title="Permalink to %3$s" rel="bookmark">permalink</a>.</span>', 'melany' );
 			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'melany' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'melany' );
-				}
-
-			} // end check for categories on this blog
+				$meta_text = __( '<span class="pull-right">Bookmark the <a href="%2$s" title="Permalink to %3$s" rel="bookmark">permalink</a>.</span>', 'melany' );
+			}
 
 			printf(
 				$meta_text,
-				$category_list,
 				$tag_list,
 				get_permalink(),
 				the_title_attribute( 'echo=0' )
 			);
 		?>
-
-		<?php edit_post_link( __( 'Edit', 'melany' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
