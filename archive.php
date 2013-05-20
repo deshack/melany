569ea9f5f20297932c7b75dcc52841182ae90ae9
deshack <deshack@linux.it>
2013-05-20 08:04:00 +0200

    Fix archive page style

--- archive.php
+++ archive.php
@@ -9,104 +9,103 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+	<section id="content" class="span9" role="main">
 
-		<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							printf( __( 'Category Archives: %s', 'melany' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-
-						elseif ( is_tag() ) :
-							printf( __( 'Tag Archives: %s', 'melany' ), '<span>' . single_tag_title( '', false ) . '</span>' );
-
-						elseif ( is_author() ) :
-							/* Queue the first post, that way we know
-							 * what author we're dealing with (if that is the case).
-							*/
-							the_post();
-							printf( __( 'Author Archives: %s', 'melany' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
-							/* Since we called the_post() above, we need to
-							 * rewind the loop back to the beginning that way
-							 * we can run the loop properly, in full.
-							 */
-							rewind_posts();
-
-						elseif ( is_day() ) :
-							printf( __( 'Daily Archives: %s', 'melany' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Monthly Archives: %s', 'melany' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Yearly Archives: %s', 'melany' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'melany' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'melany');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'melany' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'melany' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'melany' );
-
-						else :
-							_e( 'Archives', 'melany' );
-
-						endif;
-					?>
-				</h1>
+		<header class="page-header">
+			<h1 class="page-title">
 				<?php
 					if ( is_category() ) :
-						// show an optional category description
-						$category_description = category_description();
-						if ( ! empty( $category_description ) ) :
-							echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
-						endif;
+						printf( __( 'Category Archives: %s', 'melany' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 
 					elseif ( is_tag() ) :
-						// show an optional tag description
-						$tag_description = tag_description();
-						if ( ! empty( $tag_description ) ) :
-							echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
-						endif;
+						printf( __( 'Tag Archives: %s', 'melany' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+
+					elseif ( is_author() ) :
+						/* Queue the first post, that way we know
+						 * what author we're dealing with (if that is the case).
+						*/
+						the_post();
+						printf( __( 'Author Archives: %s', 'melany' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
+						/* Since we called the_post() above, we need to
+						 * rewind the loop back to the beginning that way
+						 * we can run the loop properly, in full.
+						 */
+						rewind_posts();
+
+					elseif ( is_day() ) :
+						printf( __( 'Daily Archives: %s', 'melany' ), '<span>' . get_the_date() . '</span>' );
+
+					elseif ( is_month() ) :
+						printf( __( 'Monthly Archives: %s', 'melany' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+
+					elseif ( is_year() ) :
+						printf( __( 'Yearly Archives: %s', 'melany' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+
+					elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+						_e( 'Asides', 'melany' );
+
+					elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+						_e( 'Images', 'melany');
+
+					elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+						_e( 'Videos', 'melany' );
+
+					elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+						_e( 'Quotes', 'melany' );
+
+					elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+						_e( 'Links', 'melany' );
+
+					else :
+						_e( 'Archives', 'melany' );
 
 					endif;
 				?>
-			</header><!-- .page-header -->
+			</h1>
+			<?php
+				if ( is_category() ) :
+					// show an optional category description
+					$category_description = category_description();
+					if ( ! empty( $category_description ) ) :
+						echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
+					endif;
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				elseif ( is_tag() ) :
+					// show an optional tag description
+					$tag_description = tag_description();
+					if ( ! empty( $tag_description ) ) :
+						echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
+					endif;
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				endif;
+			?>
+		</header><!-- .page-header -->
 
-			<?php endwhile; ?>
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php melany_content_nav( 'nav-below' ); ?>
+			<?php
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+			?>
 
-		<?php else : ?>
+		<?php endwhile; ?>
 
-			<?php get_template_part( 'no-results', 'archive' ); ?>
+		<?php melany_content_nav( 'nav-below' ); ?>
 
-		<?php endif; ?>
+	<?php else : ?>
 
-		</div><!-- #content -->
-	</section><!-- #primary -->
+		<?php get_template_part( 'no-results', 'archive' ); ?>
+
+	<?php endif; ?>
+
+
+	</section><!-- #content -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
