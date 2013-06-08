30d2bb0ccb2df22bc0930d8e77a9cf117a207da2
deshack <deshack@linux.it>
2013-06-08 12:51:18 +0200

    Add copy in the footer

--- footer.php
+++ footer.php
@@ -14,6 +14,8 @@
 		<div class="text-center navbar-text">
 			<small>
 				<?php do_action( 'melany_credits' ); ?>
+				<?php printf( '&copy; 2013 Mattia Migliorini' ); ?>
+				<span class="sep"> - </span>
 				<?php printf( __( 'Powered by ', 'melany' )); ?><a href="http://wordpress.org/" target="_blank" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'melany' ); ?>" rel="generator">WordPress</a><?php printf( __( ' and ', 'melany' )); ?><a href="http://twitter.github.com/bootstrap" target="_blank">Bootstrap</a>
 				<span class="sep"> - </span>
 				<?php printf( __( '%1$s theme by %2$s', 'melany' ), '<a href="https://github.com/deshack/melany" target="_blank">Melany</a>', '<a href="http://www.deshack.net/" target="_blank" rel="designer">deshack</a>' ); ?>
