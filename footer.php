5ddb384b2e5e67a395734d2524a307ba3b905aa9
deshack <deshack@linux.it>
2013-07-04 13:24:25 +0200

    Update copy and platform info

--- footer.php
+++ footer.php
@@ -13,10 +13,11 @@
 	<footer id="footer" role="contentinfo">
 		<div class="text-center navbar-text">
 			<small>
-				<?php printf( '&copy; 2013 ' ); ?>
+				<?php printf( '&copy; ' ); ?>
+				<?php echo date( 'Y ' ); ?>
 				<?php bloginfo( 'name' ); ?>
 				<span class="sep"> - </span>
-				<?php printf( __( 'Powered by ', 'melany' )); ?><a href="http://wordpress.org/" target="_blank" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'melany' ); ?>" rel="generator">WordPress</a><?php printf( __( ' and ', 'melany' )); ?><a href="http://twitter.github.com/bootstrap" target="_blank">Bootstrap</a>
+				<?php printf( __( 'Powered by ', 'melany' )); ?><a href="http://wordpress.org/" target="_blank" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'melany' ); ?>" rel="generator">WordPress</a>
 				<span class="sep"> - </span>
 				<?php printf( __( '%1$s theme by %2$s', 'melany' ), '<a href="https://github.com/deshack/melany" target="_blank">Melany</a>', '<a href="http://www.deshack.net/" target="_blank" rel="designer">deshack</a>' ); ?>
 			</small>
