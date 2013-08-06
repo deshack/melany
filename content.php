79371135e8a4556ae21ef404983f2102205ab7fc
deshack <deshack@linux.it>
2013-08-06 14:22:02 +0200

    Fix posts meta layout Fix #74

--- content.php
+++ content.php
@@ -37,26 +37,28 @@
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<footer class="well well-small row-fluid">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'melany' ) );
-				if ( $tags_list ) :
-			?>
-		<div class="span9">
-			<span>
-				<?php printf( __( 'Tags: %1$s', 'melany' ), $tags_list ); ?>
-			</span>
-		</div>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-		<div class="span3 text-right pull-right">
-			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			<?php comments_popup_link( __( 'Leave a comment', 'melany' ), __( '1 Comment', 'melany' ), __( '% Comments', 'melany' ) ); ?>
-			<?php endif; ?>
+	<footer class="well well-small">
+		<div class="row-fluid">
+			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+				<?php
+					/* translators: used between list items, there is a space after the comma */
+					$tags_list = get_the_tag_list( '', __( ', ', 'melany' ) );
+					if ( $tags_list ) :
+				?>
+			<div class="span9">
+				<span>
+					<?php printf( __( 'Tags: %1$s', 'melany' ), $tags_list ); ?>
+				</span>
+			</div>
+				<?php endif; // End if $tags_list ?>
+			<?php endif; // End if 'post' == get_post_type() ?>
+			<div class="span3 text-right pull-right">
+				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+				<?php comments_popup_link( __( 'Leave a comment', 'melany' ), __( '1 Comment', 'melany' ), __( '% Comments', 'melany' ) ); ?>
+				<?php endif; ?>
 
-			<?php edit_post_link( __( 'Edit', 'melany' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+				<?php edit_post_link( __( 'Edit', 'melany' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+			</div>
 		</div>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
