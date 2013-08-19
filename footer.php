db6be29c6ca196bc85c23fe7389ad5e10f393398
deshack <deshack@linux.it>
2013-08-19 16:22:09 +0200

    Style footer See issue #68

--- footer.php
+++ footer.php
@@ -8,21 +8,21 @@
  */
 ?>
 	</section><!-- #main -->
-
-	<footer id="footer" role="contentinfo">
-		<div class="text-center navbar-text">
-			<small>
-				<?php printf( '&copy; ' ); ?>
-				<?php echo date( 'Y ' ); ?>
-				<?php bloginfo( 'name' ); ?>
-				<span class="sep"> - </span>
-				<?php printf( __( 'Powered by ', 'melany' )); ?><a href="http://wordpress.org/" target="_blank" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'melany' ); ?>">WordPress</a>
-				<span class="sep"> - </span>
-				<?php printf( __( '%1$s theme by %2$s', 'melany' ), '<a href="https://github.com/deshack/melany" target="_blank">Melany</a>', '<a href="http://www.deshack.net/" target="_blank">deshack</a>' ); ?>
-			</small>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
+
+<footer id="footer" class="navbar navbar-inverse" role="contentinfo">
+	<div class="navbar-text text-center">
+		<small>
+			<?php printf( '&copy; ' ); ?>
+			<?php echo date( 'Y ' ); ?>
+			<?php bloginfo( 'name' ); ?>
+			<span class="sep"> - </span>
+			<?php printf( __( 'Powered by ', 'melany' )); ?><a href="http://wordpress.org/" target="_blank" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'melany' ); ?>">WordPress</a>
+			<span class="sep"> - </span>
+			<?php printf( __( '%1$s theme by %2$s', 'melany' ), '<a href="https://github.com/deshack/melany" target="_blank">Melany</a>', 'deshack' ); ?>
+		</small>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
