bb48560349616170749e0735aecccb92cf3fd71b
deshack <deshack@linux.it>
2013-08-20 16:10:40 +0200

    Display entry meta in panels instead of wells

--- content.php
+++ content.php
@@ -37,28 +37,30 @@
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<footer class="well well-small">
-		<div class="row">
-			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-				<?php
-					/* translators: used between list items, there is a space after the comma */
-					$tags_list = get_the_tag_list( '', __( ', ', 'melany' ) );
-					if ( $tags_list ) :
-				?>
-			<div class="col-sm-9 col-lg-9 tag-list">
-				<span>
-					<?php printf( __( 'Tags: %1$s', 'melany' ), $tags_list ); ?>
-				</span>
-			</div>
-				<?php endif; // End if $tags_list ?>
-			<?php endif; // End if 'post' == get_post_type() ?>
-			<div class="col-sm-3 col-lg-3 leave-comment">
-				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-				<?php comments_popup_link( __( 'Leave a comment', 'melany' ), __( '1 Comment', 'melany' ), __( '% Comments', 'melany' ) ); ?>
-				<?php endif; ?>
+	<footer class="panel panel-default">
+		<div class="panel-body">
+			<div class="row">
+				<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+					<?php
+						/* translators: used between list items, there is a space after the comma */
+						$tags_list = get_the_tag_list( '', __( ', ', 'melany' ) );
+						if ( $tags_list ) :
+					?>
+				<div class="col-sm-9 col-lg-9 tag-list">
+					<span>
+						<?php printf( __( 'Tags: %1$s', 'melany' ), $tags_list ); ?>
+					</span>
+				</div>
+					<?php endif; // End if $tags_list ?>
+				<?php endif; // End if 'post' == get_post_type() ?>
+				<div class="col-sm-3 col-lg-3 leave-comment">
+					<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+					<?php comments_popup_link( __( 'Leave a comment', 'melany' ), __( '1 Comment', 'melany' ), __( '% Comments', 'melany' ) ); ?>
+					<?php endif; ?>
 
-				<?php edit_post_link( __( 'Edit', 'melany' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+					<?php edit_post_link( __( 'Edit', 'melany' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+				</div>
 			</div>
-		</div>
+		</div><!-- .panel-body -->
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
