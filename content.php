35df6ddd7d10f5923d4018c91448b46bb625160a
deshack <deshack@linux.it>
2013-08-26 13:53:20 +0200

    Add missing classes for Rich Snippets support Fix #122

--- content.php
+++ content.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'hero-unit' ); ?>>
 	<header class="page-header">
-		<h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 		
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta muted">
