1218610bcc86b45792044ade98e010f6da510274
deshack <deshack@linux.it>
2013-08-27 13:58:07 +0200

    Update template-tags.php functions Close #143

--- image.php
+++ image.php
@@ -8,8 +8,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area image-attachment">
-		<div id="content" class="site-content" role="main">
+	<section id="primary" class="content-area image-attachment">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
@@ -23,18 +22,17 @@ get_header();
 							printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', 'melany' ),
 								esc_attr( get_the_date( 'c' ) ),
 								esc_html( get_the_date() ),
-								wp_get_attachment_url(),
+								esc_url( wp_get_attachment_url() ),
 								$metadata['width'],
 								$metadata['height'],
-								get_permalink( $post->post_parent ),
+								esc_url( get_permalink( $post->post_parent ) ),
 								esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
 								get_the_title( $post->post_parent )
 							);
-						?>
-						<?php edit_post_link( __( 'Edit', 'melany' ), '<span class="sep"> | </span> <span class="edit-link">', '</span>' ); ?>
+						edit_post_link( __( 'Edit', 'melany' ), '<span class="edit-link">', '</span>' ); ?>
 					</div><!-- .entry-meta -->
 
-					<nav role="navigation" id="image-navigation" class="navigation-image">
+					<nav role="navigation" id="image-navigation" class="image-navigation">
 						<div class="nav-previous"><?php previous_image_link( false, __( '<span class="meta-nav">&larr;</span> Previous', 'melany' ) ); ?></div>
 						<div class="nav-next"><?php next_image_link( false, __( 'Next <span class="meta-nav">&rarr;</span>', 'melany' ) ); ?></div>
 					</nav><!-- #image-navigation -->
@@ -44,53 +42,18 @@ get_header();
 
 					<div class="entry-attachment">
 						<div class="attachment">
-							<?php
-								/**
-								 * Grab the IDs of all the image attachments in a gallery so we can get the URL of the next adjacent image in a gallery,
-								 * or the first image (if we're looking at the last image in a gallery), or, in a gallery of one, just the link to that image file
-								 */
-								$attachments = array_values( get_children( array(
-									'post_parent'    => $post->post_parent,
-									'post_status'    => 'inherit',
-									'post_type'      => 'attachment',
-									'post_mime_type' => 'image',
-									'order'          => 'ASC',
-									'orderby'        => 'menu_order ID'
-								) ) );
-								foreach ( $attachments as $k => $attachment ) {
-									if ( $attachment->ID == $post->ID )
-										break;
-								}
-								$k++;
-								// If there is more than 1 attachment in a gallery
-								if ( count( $attachments ) > 1 ) {
-									if ( isset( $attachments[ $k ] ) )
-										// get the URL of the next image attachment
-										$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-									else
-										// or get the URL of the first image attachment
-										$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
-								} else {
-									// or, if there's only 1 image, get the URL of the image
-									$next_attachment_url = wp_get_attachment_url();
-								}
-							?>
-
-							<a href="<?php echo $next_attachment_url; ?>" title="<?php the_title_attribute(); ?>" rel="attachment"><?php
-								$attachment_size = apply_filters( 'melany_attachment_size', array( 1200, 1200 ) ); // Filterable image size.
-								echo wp_get_attachment_image( $post->ID, $attachment_size );
-							?></a>
+							<?php melany_the_attached_image(); ?>
 						</div><!-- .attachment -->
 
-						<?php if ( ! empty( $post->post_excerpt ) ) : ?>
+						<?php if ( has_excerpt() ) : ?>
 						<div class="entry-caption">
 							<?php the_excerpt(); ?>
 						</div><!-- .entry-caption -->
 						<?php endif; ?>
 					</div><!-- .entry-attachment -->
 
-					<?php the_content(); ?>
 					<?php
+						the_content();
 						wp_link_pages( array(
 							'before' => '<div class="page-links">' . __( 'Pages:', 'melany' ),
 							'after'  => '</div>',
@@ -100,18 +63,18 @@ get_header();
 				</div><!-- .entry-content -->
 
 				<footer class="entry-meta">
-					<?php if ( comments_open() && pings_open() ) : // Comments and trackbacks open ?>
-						<?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'melany' ), get_trackback_url() ); ?>
-					<?php elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open ?>
-						<?php printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'melany' ), get_trackback_url() ); ?>
-					<?php elseif ( comments_open() && ! pings_open() ) : // Only comments open ?>
-						<?php _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'melany' ); ?>
-					<?php elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed ?>
-						<?php _e( 'Both comments and trackbacks are currently closed.', 'melany' ); ?>
-					<?php endif; ?>
-					<?php edit_post_link( __( 'Edit', 'melany' ), ' <span class="edit-link">', '</span>' ); ?>
+					<?php if ( comments_open() && pings_open() ) : // Comments and trackbacks open
+						printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'melany' ), esc_url( get_trackback_url() ) );
+					elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open
+						printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'melany' ), esc_url( get_trackback_url() ) );
+					elseif ( comments_open() && ! pings_open() ) : // Only comments open
+						_e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'melany' );
+					elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed
+						_e( 'Both comments and trackbacks are currently closed.', 'melany' );
+					endif;
+					edit_post_link( __( 'Edit', 'melany' ), ' <span class="edit-link">', '</span>' ); ?>
 				</footer><!-- .entry-meta -->
-			</article><!-- #post-<?php the_ID(); ?> -->
+			</article><!-- #post-## -->
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
@@ -121,7 +84,6 @@ get_header();
 
 		<?php endwhile; // end of the loop. ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+	</section><!-- #primary -->
 
 <?php get_footer(); ?>
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -188,6 +188,7 @@ function melany_content_nav( $nav_id ) {
 
 	<?php if ( is_single() ) : // navigation links for single posts ?>
 
+		<h1 class="sr-only"><?php _e( 'Post navigation', 'melany' ); ?></h1>
 		<ul class="pager">
 		<?php
 			$previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
@@ -403,6 +404,58 @@ function melany_avatar_class( $output ) {
 }
 add_filter( 'get_avatar', 'melany_avatar_class' );
 endif;
+
+if ( ! function_exists( 'melany_the_attached_image' ) ) :
+/**
+ * Prints the attached image with a link to the next attached image
+ */
+function melany_the_attached_image() {
+	$post									= get_post();
+	$attachment_size			= apply_filters( 'melany_attachment_size', array( 1200, 1200 ) );
+	$next_attachment_url	= wp_get_attachment_url();
+
+	/**
+	 * Grab the IDs of all the image attachments in a gallery so we can get the 
+	 * URL of the next adjacent image in a gallery, or the first image (if
+	 * we're looking at the last image in a gallery), or, in a gallery of one,
+	 * just the link to that image file.
+	 */
+	$attachment_ids = get_post( array(
+		'post_parent'			=> $post->post_parent,
+		'fields'					=> 'ids',
+		'numberposts'			=> -1,
+		'post_status'			=> 'inherit',
+		'post_type'				=> 'attachment',
+		'post_mime_type'	=> 'image',
+		'order'						=> 'ASC',
+		'orderby'					=> 'menu_order ID'
+	) );
+
+	// If there is more than 1 attachment in a gallery
+	if ( count( $attachment_ids ) > 1 ) {
+		foreach ( $attachment_ids as $attachment_id ) {
+			if ( $attachment_id == $post->ID ) {
+				$next_id = current ( $attachment_ids );
+				break;
+			}
+		}
+
+		// Get the URL of the next image attachment
+		if ( $next_id )
+			$next_attachment_url = get_attachment_link( $next_id );
+
+		// or get the URL of the first image attachment
+		else
+			$next_attachment_url = get_attachment_link( array_shift( $attachment_ids ) );
+	}
+
+	printf( '<a href="%1$s" title="%2$s" rel="attachment">%3$s</a>',
+		esc_url( $next_attachment_url ),
+		the_title_attribute( array( 'echo' => false ) ),
+		wp_get_attachment_image( $post->ID, $attachment_size )
+	);
+}
+endif; // Ends check for melany_the_attached_image
 
 if ( ! function_exists( 'melany_posted_on' ) ) :
 /**
