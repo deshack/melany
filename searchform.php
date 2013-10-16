c6ff199fda7d3a0127a3943c6c3c794b7a598ace
deshack <deshack@linux.it>
2013-10-16 16:47:29 +0200

    Fix broken search form Fix #164

--- searchform.php
+++ searchform.php
@@ -7,6 +7,6 @@
 ?>
 <form method="get" class="search-form navbar-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label class="sr-only"><?php _ex( 'Search:', 'label', 'melany' ); ?></label>
-	<input type="search" class="form-control" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', 'melany' ); ?>" />
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'melany' ); ?>">
+	<input type="search" id="s" name="s" class="form-control" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', 'melany' ); ?>" />
+	<input type="submit" for="s" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'melany' ); ?>">
 </form>
