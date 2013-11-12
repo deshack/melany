133df27efcb01b8c9e8c4034bb20729922da3b6e
deshack <deshack@linux.it>
2013-11-12 15:00:02 +0100

    Sidebar is blank by default Fix #179

--- sidebar.php
+++ sidebar.php
@@ -8,26 +8,5 @@
 	<section id="secondary" class="widget-area col-md-3" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-			<aside id="search" class="widget widget_search">
-				<?php get_search_form(); ?>
-			</aside>
-
-			<aside id="archives" class="widget">
-				<h3 class="widget-title"><?php _e( 'Archives', 'melany' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
-
-			<aside id="meta" class="widget">
-				<h3 class="widget-title"><?php _e( 'Meta', 'melany' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
-
 		<?php endif; // end sidebar widget area ?>
 	</section><!-- #secondary -->
