2caab18206448f2727d97a2130450ce8d26f2dd3
Mattia Migliorini <deshack@ubuntu.com>
2014-01-04 19:16:39 +0100

    Remove custom background feature

--- functions.php
+++ functions.php
@@ -61,14 +61,6 @@ function melany_setup() {
 	 * Add new image sizes
 	 */
 	add_image_size( 'single_post_thumb', 800, 300, true );
-
-	/**
-	 * Setup the WordPress core custom background feature.
-	 */
-	add_theme_support( 'custom-background', apply_filters( 'melany_custom_background_args', array(
-		'default-color'	=> 'ffffff',
-		'default-image'	=> '',
-	) ) );
 }
 add_action( 'after_setup_theme', 'melany_setup' );
 endif; // melany_setup
