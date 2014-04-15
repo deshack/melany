89d48ea526b360030d0cd794c8411eaed217135f
Mattia Migliorini <deshack@ubuntu.com>
2014-04-15 12:00:49 +0200

    Add "Back to home" button to 404 page. See #31

--- 404.php
+++ 404.php
@@ -18,20 +18,26 @@ get_header(); ?>
 				<img src="<?php echo get_template_directory_uri() . '/img/404.png'; ?>" class="aligncenter img-rounded" alt="404">
 				<p class="text-center"><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'melany' ); ?></p>
 
-			<section id="widgets404">
-				<form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-					<div class="input-group">
-						<label class="sr-only"><?php _e( 'Search:', 'melany' ); ?></label>
-						<input type="search" id="s" name="s" class="form-control" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="<?php echo esc_attr( __('Search&hellip;', 'melany') ); ?>" />
-						<span class="input-group-btn">
-							<button type="submit" for="s" class="btn btn-default">
-								<span class="sr-only"><?php _e( 'Search', 'melany' ); ?></span>
-								<span class="glyphicon glyphicon-search"></span>
-							</button>
-						</span>
-					</div>
-				</form>
-			</section><!-- #widgets404 -->
+				<section id="widgets404">
+					<form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+						<div class="input-group">
+							<label class="sr-only"><?php _e( 'Search:', 'melany' ); ?></label>
+							<input type="search" id="s" name="s" class="form-control" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="<?php echo esc_attr( __('Search&hellip;', 'melany') ); ?>" />
+							<span class="input-group-btn">
+								<button type="submit" for="s" class="btn btn-success">
+									<span class="sr-only"><?php _e( 'Search', 'melany' ); ?></span>
+									<span class="glyphicon glyphicon-search"></span>
+								</button>
+							</span>
+						</div>
+					</form>
+
+					<div class="row">
+						<div class="col-sm-6 col-sm-offset-3">
+							<button type="button" class="btn btn-success btn-lg btn-block"><?php _e( 'Back to home', 'melany' ); ?></button>
+						</div>
+					</div><!-- .row -->
+				</section><!-- #widgets404 -->
 
 			</div><!-- .entry-content -->
 		</article><!-- #post-0 .post .error404 .not-found -->
